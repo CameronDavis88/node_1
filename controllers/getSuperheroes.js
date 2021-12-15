31234c69c0296d1397501daf7f8beab9b646e04b
CameronDavis88 <cammien88@gmail.com>
2021-12-15 14:26:23 -0700

    Added controllers and tested endpoints

---
+++ controllers/getSuperheroes.js
@@ -0,0 +1,61 @@
+
+//pretend database for the purpose of this practice
+let superHeroes = [
+    {name: 'Super Man', power: 'Web-slinging'},
+     {name: 'Bat Man', power: 'Technology'},
+     {name: 'Aqua Man', power: 'Breath-holding'},
+     {name: 'Thor', power: "Flying-ish"}
+    ] 
+
+
+    let getSuperheroes = (req, res) => {
+        let responseHeroes = superHeroes;
+        if (req.query.hero) {
+            responseHeroes = superHeroes.filter(e => e.toLowerCase() === req.query.hero.toLowerCase())[0]
+        }
+        res.status(200).json(responseHeroes)
+    }
+   
+    let getHero = (req, res) => {
+        let superHero = {}
+        console.log(req.params)
+        for (let key in superHeroes){
+            if(superHeroes[key].name === req.params.name){
+                superHero = superHeroes[key]
+            }
+    }
+    res.status(200, console.log('200')).json(superHero)
+    
+    }
+    
+    let addHero = (req, res) => {
+        superHeroes.push(req.body)
+        res.status(200).json(superHeroes)
+    }
+    
+    let editHero = (req, res) => {
+        for (let key in superHeroes){
+            if(superHeroes[key].name === req.body.name)
+            superHeroes.splice(key, 1, req.body)
+            console.log(superHeroes[key].name)
+        }
+        res.status(200).json(superHeroes)
+    }
+    
+    let deleteHero = (req, res) => {
+        for (let key in superHeroes){
+            if(superHeroes[key].name === req.params.name)
+            superHeroes.splice(key, 1)
+        }
+        res.status(200).json(superHeroes)
+    }
+
+
+//You could just export the names of the functions or do module.exports for the whole thing but it needs to be an object
+module.exports = {
+getSuperheroes,
+getHero,
+addHero,
+editHero,
+deleteHero,
+}
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // this is the old way of importing that has to be used when using Node in th backend
 const express = require('express');
+const {
+    getSuperheroes,
+    getHero,
+    addHero,
+    editHero,
+    deleteHero,
+} = require('./controllers/getSuperheroes')
 const portNum = 3333
 
 // basically this is your server now, makes an express app
@@ -11,37 +18,20 @@ const app = express();
 //allows us to access the body of the requests that are sent in
 app.use(express.json());
 
-let superHeroes = ['Super Man', 'Bat Man', 'Aqua Man', 'Thanos', 'Thor'] //pretend database for the purpose of this practice
+
 
 //endpoints:
-//for query:
-app.get(`/api/superheroes`, (req, res, next) => {
-    let responseHeroes = superHeroes;
-    if (req.query.hero) {
-        responseHeroes = superHeroes.filter(e => e.toLowerCase() === req.query.hero.toLowerCase())[0]
-    }
-    res.status(200).json(responseHeroes)
+app.get(`/api/superheroes`, getSuperheroes)
 
-})
-
-app.get(`/api/superheroes`, (req, res, next) => {
-    res.status(200, console.log('200')).json(superHeroes)
-    //res.send or res.json sends the thing inside as the response -- res.json forces the object to be in json format
-    //make sure your status etc are all before the send or json!
-})
 
 // check out on :  http://localhost:3333/api/superheroes/Thanos
-app.get(`/api/superheroes/:name`, (req, res, next) => {
-    console.log(req.params) // this will only show up in your server's console, not the browser's
-    const superHero = superHeroes.filter(e => e.toLowerCase() === req.params.name.toLowerCase)[0]
-    res.status(200, console.log('200')).json(superHero)
+app.get(`/api/superheroes/:name`, getHero)
 
-})
+app.post(`/api/superheroes`, addHero)
 
+app.put(`/api/superheroes`, editHero)
 
-// app.post(`/api/superheroes`)
-// app.put(`/api/superheroes`)
-// app.delete(`/api/superheroes`)
+app.delete(`/api/superheroes/:name`, deleteHero)
 
 
 
