8285376b66f3283a451f186c87647d8112fefd96
CameronDavis88 <cammien88@gmail.com>
2021-12-15 18:06:50 -0700

    made more dynamic etc. especially the editHeros method. Also set id's to count, making them  dynamically unique.

--- controllers/getSuperheroes.js
+++ controllers/getSuperheroes.js
@@ -1,58 +1,53 @@
-
-//pretend database for the purpose of this practice
-let superHeroes = [
-    {name: 'Super Man', power: 'Web-slinging'},
-     {name: 'Bat Man', power: 'Technology'},
-     {name: 'Aqua Man', power: 'Breath-holding'},
-     {name: 'Thor', power: "Flying-ish"},
-    ] 
-
-    let getSuperheroes = (req, res) => {
-        let responseHeroes = superHeroes;
-        if (req.query.hero) {
-            responseHeroes = superHeroes.filter(e => e.toLowerCase() === req.query.hero.toLowerCase())[0]
-        }
-        res.status(200).json(responseHeroes)
-    }
-   
-    let getHero = (req, res) => {
-        let superHero = {}
-        for (let key in superHeroes){
-            if(superHeroes[key].name === req.params.name){
-                superHero = superHeroes[key]
-            }
-    }
-    res.status(200, console.log('200')).json(superHero)
-    
-    }
-    
-    let addHero = (req, res) => {
-        superHeroes.push(req.body)
-        res.status(200).json(superHeroes)
-    }
-    
-    let editHero = (req, res) => {
-        for (let key in superHeroes){
-            if(superHeroes[key].name === req.body.name)
-            superHeroes.splice(key, 1, req.body)
-        }
-        res.status(200).json(superHeroes)
-    }
-    
-    let deleteHero = (req, res) => {
-        for (let key in superHeroes){
-            if(superHeroes[key].name === req.params.name)
-            superHeroes.splice(key, 1)
-        }
-        res.status(200).json(superHeroes)
-    }
+const {superHeroes} = require('../db/db');
+let count = superHeroes.length + 1;
 
 
-//You could just export the names of the functions or do module.exports for the whole thing but it needs to be an object
+let getSuperheroes = (req, res) => {
+    let responseHeroes = superHeroes;
+    res.status(200).json(responseHeroes);
+    console.log(count)
+}
+
+let getHero = (req, res) => {
+    let superHero = {}
+    for (let key in superHeroes) {
+        if (superHeroes[key].name.toLowerCase() === req.params.name.toLowerCase()) {
+            superHero = superHeroes[key]
+        } 
+    }
+    res.status(200, console.log('200')).json(superHero);
+}
+
+let addHero = (req, res) => {
+    superHeroes.push(req.body);
+    req.body.id = count;
+    count++
+    res.status(200).json(superHeroes);
+}
+
+let editHero = (req, res) => {
+    let newHero = req.body;
+    for (let key in superHeroes){
+    if (superHeroes[key].id === req.body.id) {
+        superHeroes.splice(key, 1, newHero);
+    }
+}
+res.status(200).json(superHeroes);
+}
+
+let deleteHero = (req, res) => {
+    for (let key in superHeroes) {
+        if (superHeroes[key].name.toLowerCase() === req.params.name.toLowerCase()){
+            superHeroes.splice(key, 1);
+        } 
+    }
+    res.status(200).json(superHeroes);
+}
+
 module.exports = {
-getSuperheroes,
-getHero,
-addHero,
-editHero,
-deleteHero,
+    getSuperheroes,
+    getHero,
+    addHero,
+    editHero,
+    deleteHero,
 }
---
+++ db/db.js
@@ -0,0 +1,26 @@
+//pretend database for the purpose of this practice
+let superHeroes = [
+    {
+        name: 'Super Man',
+        power: 'Web-slinging',
+        id: 1,
+    },
+    {
+        name: 'Bat Man',
+        power: 'Technology',
+        id: 2,
+    },
+    {
+        name: 'Aqua Man',
+        power: 'Breath-holding',
+        id: 3,
+    },
+    {
+        name: 'Thor',
+        power: "Flying-ish",
+        id: 4,
+    },
+]
+module.exports = {
+    superHeroes,
+}
